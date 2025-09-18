1a8ff2f7fd1f380ca0ff35cdc9c9b7fdb1fba311
Bruno <bruno.rsilva2004@gmail.com>
2025-09-18 19:39:19 -0300

    correções para teste

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -16,10 +16,11 @@ interface User {
 }
 
 interface RegisterData {
-    email: string;
     first_name: string;
     last_name: string;
+    email: string;
     password: string;
+    password2: string; 
 }
 
 interface AuthContextType {
